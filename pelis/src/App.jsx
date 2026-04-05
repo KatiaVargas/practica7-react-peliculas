502994df83fa8ec6a02379125cb8b3321a5f7748
Katia Paola Vargas Flores <katia.vargas6025@alumnos.udg.mx>
2026-04-04 23:28:44 -0500

    Últimos toques, fixes y píe de página

--- pelis/src/App.jsx
+++ pelis/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import {AppRouter} from "./routers/AppRouter";
 
@@ -7,9 +6,17 @@ function App() {
   return (
     <div>    
       <header>
-      <h1 className='title'>Películas</h1>
+      <h1 className='title'>. ݁₊ ⊹ . ݁ ⟡ ݁ . ⊹ ₊ ݁. Películas . ݁₊ ⊹ . ݁ ⟡ ݁ . ⊹ ₊ ݁.</h1>
       </header>
       <AppRouter />
+
+      <footer className="pie_pagina">
+        || Alumna: Katia Paola Vargas Flores
+        <p>|| Profesor: Zeus Emanuel Gutierrez Cobian</p>
+        <p>|| Materia: Dessarrollo de aplicaciones web en la nube y móviles</p>
+        <p>|| Ciclo escolar: 2026A       Abril - 2026</p>
+        <img src="/img/logo.png" alt="Logo" className="logo" />
+      </footer>
     </div>
   ); 
 }
